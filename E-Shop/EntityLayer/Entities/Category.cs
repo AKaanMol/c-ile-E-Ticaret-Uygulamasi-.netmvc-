75e735c44e038115a1e93e5c2294aa3819f2d742
AKaanMol <kaanmol@gmail.com>
2023-01-15 00:53:05 +0300

    C# ile E-ticaret uygulamasi
    C# web uygulaması. Kullanıcı girişi, admin girişi ayrılmıştır. Admin sayfası özelinde güncelleme ve silme vb. mevcuttur. Kullanıcı girişi yapılmayan girişlerde belirli kısıtlamalar yapılmıştır ve mail onayı ile üyelik alınıyor. İçindekiler: Katmanlı mimari, Generic Repository, CodeFirst,  Entity Framework, Ado.Net

---
+++ E-Shop/EntityLayer/Entities/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Entities
+{
+    public class Category
+    {
+
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Boş Geçilemez")]
+        [Display(Name = "İsim")]
+        [StringLength(50, ErrorMessage = "Maksimum 50 karakter girebilirsiniz.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Boş Geçilemez")]
+        [Display(Name = "Açıklama")]
+        [StringLength(50, ErrorMessage = "Maksimum 50 karakter girebilirsiniz.")]
+        public string Description { get; set; }
+
+        public virtual List<Product> Products { get; set; }
+    }
+}
---
+++ E-Shop/EntityLayer/Entities/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace EntityLayer.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Boş Geçilemez")]
+        [Display(Name = "İsim")]
+        [StringLength(50, ErrorMessage = "Maksimum 50 karakter girebilirsiniz.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Boş Geçilemez")]
+        [Display(Name = "Soyisim")]
+        [StringLength(50, ErrorMessage = "Maksimum 50 karakter girebilirsiniz.")]
+        public string SurName { get; set; }
+        [Required(ErrorMessage = "Boş Geçilemez")]
+        [Display(Name = "Email")]
+        [StringLength(50, ErrorMessage = "Maksimum 50 karakter girebilirsiniz.")]
+        [EmailAddress(ErrorMessage = "E-mail formatında olmalıdır.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Boş Geçilemez")]
+        [Display(Name = "Kullanıcı Adı")]
+        [StringLength(50, ErrorMessage = "Maksimum 50 karakter girebilirsiniz.")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Boş Geçilemez")]
+        [Display(Name = "Şifre")]
+        [StringLength(50, ErrorMessage = "Maksimum 50 karakter girebilirsiniz.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Boş Geçilemez")]
+        [Display(Name = "Şifre")]
+        [StringLength(50, ErrorMessage = "Maksimum 50 karakter girebilirsiniz.")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Şifreler uyuşmuyor.")]
+        public string RePassword { get; set; }
+        [StringLength(10, ErrorMessage = "Maksimum 10 karakter girebilirsiniz.")]
+        public string Role { get; set; }
+
+
+
+    }
+}
